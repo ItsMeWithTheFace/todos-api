a1ac422b3c6c313f44120c09a00ed15be135a20c
rakinuddin <rakin.uddin@mail.utoronto.ca>
2018-10-15 01:07:49 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # items is nested to enforce 1:m association at routing level
+  resources :todos do
+    resources :items
+  end
 end
