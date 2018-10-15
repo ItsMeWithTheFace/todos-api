71850d792b15e8f60d2e9750e817017668b8fd70
rakinuddin <rakin.uddin@mail.utoronto.ca>
2018-10-15 01:06:14 -0400

    fixed todos api tests

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -98,7 +98,7 @@ RSpec.describe 'Todos API', type: :request do
 
   # test suite for DELETE /todos/:id
   describe 'DELETE /todos/:id' do
-    before { delete "todos/#{todo_id}" }
+    before { delete "/todos/#{todo_id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
